39792d1ff60be98b94a51f1bed48ddf0f5853ee3
miguel <mviamonte82@gmail.com>
2023-11-14 09:14:03 +0100

    Release 1.1 README notes

--- network.tf
+++ network.tf
@@ -4,8 +4,8 @@ resource "digitalocean_vpc" "acme-vpc-single-region" {
   ip_range = var.vpc-cidr
 }
 
-# resource "digitalocean_vpc" "acme-vpc-dynamic-multi-region" {
-#   count  = length(local.dynamic-regions)
-#   name   = "vpc-${var.customer-name}-${var.project-name}-${count.index}-${var.deployed-by}"
-#   region = local.dynamic-regions[count.index]
-# }
+resource "digitalocean_vpc" "acme-vpc-dynamic-multi-region" {
+  count  = length(local.dynamic-regions)
+  name   = "vpc-${var.customer-name}-${var.project-name}-${count.index}-${var.deployed-by}"
+  region = local.dynamic-regions[count.index]
+}
