4e07b057742884f3a491f7413a36dc62dc20138e
miguel <mviamonte82@gmail.com>
2023-12-01 08:25:51 +0100

    Terraform format

--- networking/vpc/vpc.tf
+++ networking/vpc/vpc.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_vpc" "acme-vpc-nyc-region" {
-  count = length(var.regions)
+  count    = length(var.regions)
   name     = "vpc-${var.customer-name}-${var.deployed-by}-${var.regions[count.index]}"
   region   = var.regions[count.index]
   ip_range = local.ip_nets[count.index]
