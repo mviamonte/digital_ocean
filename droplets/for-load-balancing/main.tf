4de1138d95b4a29399bf853e043ed9634a1ab347
miguel <mviamonte82@gmail.com>
2023-12-06 14:56:09 +0100

    Latest changes to main.tf

--- droplets/for-load-balancing/main.tf
+++ droplets/for-load-balancing/main.tf
@@ -1,16 +1,16 @@
 #Using dynamic list with specific features for the regions
 
 resource "digitalocean_droplet" "mz-multiple-droplet-single-region" {
-  for_each      = local.mz-map
+  count         = 2
   user_data     = file("scripts/userdata.yml")
   image         = var.image
-  name          = "droplet-${var.customer-name}-${var.project-name}-${each.key}-${var.deployed-by}"
+  name          = "droplet-${var.customer-name}-${var.project-name}-${count.index}-${var.deployed-by}"
   size          = var.droplet_size
   ssh_keys      = [digitalocean_ssh_key.default.fingerprint]
   droplet_agent = true
   tags          = local.tags #This attribute expect a list of string
-  region        = each.key
-  vpc_uuid      = each.value
+  region        = local.dynamic-regions[0]
+  vpc_uuid      = local.vpc-uuid-list[0]
 }
 
 #Creating the SSH resource for the droplet
