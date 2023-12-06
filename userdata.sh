cff259f3ef82eb8f13744bb7b4969ebf9d92cc54
miguel <mviamonte82@gmail.com>
2023-12-06 12:07:31 +0100

    Deleting files from main directory

--- userdata.sh
+++
@@ -1,7 +0,0 @@
-#Install Packages
-sudo apt update && apt -y upgrade
-sudo apt install -y apache2 #With auto approve
-sudo apt install -y curl #With auto approve
-#Enable firewall
-sudo ufw allow 'Apache'
-sudo ufw enable #Add the automatic -y option
